3c35ea97bc80e0b4a9a51147de607b639ef4e381
Greg Taylor <gtaylor@duointeractive.com>
2012-05-15 23:47:02 -0400

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,21 @@
+EVE Market Data Structures
+==========================
+
+:Author: Greg Taylor
+:License: BSD
+
+This project contains a set of data structures used to represent EVE Market
+data. Also included are a set of serializers and de-serializers, dealing with
+the common market formats. Currently, this is just
+Unified Uploader Interchange Format.
+
+Documentation
+-------------
+
+Forthcoming.
+
+License
+-------
+
+This project, and all contributed code, are licensed under the BSD License.
+A copy of the BSD License may be found in the repository.
---
+++ setup.py
@@ -0,0 +1,42 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import emds
+
+required = [
+    'python-dateutil<2.0',
+    'pytz',
+]
+
+packages = [
+    'emds',
+]
+
+scripts = [
+]
+
+setup(
+    name='emds',
+    version=emds.__version__,
+    description='EVE Market Data Relay',
+    long_description=open('README.rst').read(),
+    author='Greg Taylor',
+    author_email='gtaylor@gc-taylor.com',
+    url='https://github.com/gtaylor/EVE-Market-Data-Structures',
+    packages=packages,
+    scripts=scripts,
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=required,
+    license='BSD',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        ),
+    )
