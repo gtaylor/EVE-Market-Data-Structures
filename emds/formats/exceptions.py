6c645b69b91b6a29d4e8786c07e6438d16667415
Greg Taylor <gtaylor@duointeractive.com>
2012-07-06 23:40:18 -0400

    Make ParseError a child of EMDSError.

--- emds/formats/exceptions.py
+++ emds/formats/exceptions.py
@@ -1,8 +1,9 @@
 """
 Various parser-related exceptions.
 """
+from emds.exceptions import EMDSError
 
-class ParseError(Exception):
+class ParseError(EMDSError):
     """
     Raise this when some unrecoverable error happens while parsing serialized
     market data.
